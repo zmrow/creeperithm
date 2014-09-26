d88121a18287955561c16a381e951943834e5df0
Zac Mrowicki <zmrowicki@hotmail.com>
2014-09-25 19:36:34 -0700

    Basic grid CSS and HTML. Starting to build grid colors for initial face

---
+++ script.js
@@ -0,0 +1,20 @@
+function Blocks() {
+  this.colors = ['G', 'B'];
+  this.gPositions = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','20','21','24,'25','28','29','32','33','34','35','38',39','40','41','42','47','48','49','50','55','56','57','58','60','61','63','64'];
+  this.bPositions = 
+  
+  for (i = 0; i < 64; i++) {
+    $('.sortContainer').append('<div class="square"></div');    
+  }
+}
+
+$(document).ready(function() {
+  for (i = 0; i < 64; i++) {
+    $('#scrambled').append('<div class="square"></div');    
+  }
+})
+
+//blck = 20
+//g = 44
+
+
