3f261914b509f9a549f31c4ea0d56aee37cf02a8
Zac Mrowicki <zmrowicki@hotmail.com>
2014-09-28 18:01:01 -0700

    Cleaning up a bit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function Creeper() {
   this.colors = ['G', 'B'];
-  this.gPositions = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','19','20','23','24','27','28','31','32','33','34','37','38','39','40','41','46','47','48','49','54','55','56','57','59','60','62','63',];
+  this.gPositions = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','19','20','23','24','27','28','31','32','33','34','37','38','39','40','41','46','47','48','49','54','55','56','57','59','60','62','63'];
   this.bPositions = ['17','18','21','22','25','26','29','30','35','36','42','43','44','45','50','51','52','53','58','61'];
 
   var theBlock = this;
@@ -16,9 +16,11 @@ function Creeper() {
       $.each(theBlock.bPositions, function(index, position) {
         var block = new Block(color, position);
         $('#scrambled').append(block.toHTML());
+        console.log(block);
       });
-    };
+    }
   });
+}
 
 function Block(color, position) {
   this.color = color;
@@ -29,9 +31,6 @@ function Block(color, position) {
 }
 
 $(document).ready(function() {
- // for (i = 0; i < 64; i++) {
- //   $('#scrambled').append('<div class="square"></div');    
- // }
  var creeper = new Creeper();
 });
 
