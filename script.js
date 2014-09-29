70b600757cf79e368fcb2d4303d6f296a396d1e4
Zac Mrowicki <zmrowicki@hotmail.com>
2014-09-28 17:46:02 -0700

    Finish create blocks, correct colors in CSS

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 function Creeper() {
   this.colors = ['G', 'B'];
-  this.gPositions = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','20','21','24','25','28','29','32','33','34','35','38','39','40','41','42','47','48','49','50','55','56','57','58','60','61','63','64'];
-  this.bPositions = ['18','19','22','23','26','27','30','31','36','37','43','44','45','46','51','52','53','54','59','62'];
+  this.gPositions = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','19','20','23','24','27','28','31','32','33','34','37','38','39','40','41','46','47','48','49','54','55','56','57','59','60','62','63',];
+  this.bPositions = ['17','18','21','22','25','26','29','30','35','36','42','43','44','45','50','51','52','53','58','61'];
 
   var theBlock = this;
 
-  // Should have positions in the array.  Then assign a color based on position?
-  // Or could assign a certain color based on position.
-  //
-  // Example:
-  // each (position)
-  //  if position in g position
-  //    give it color G
-  
   $.each(this.colors, function(index, color) {
-    if (this.color == 'G') {
-      $.each(theBlock.gPositions, function(index, gposition) {
-        var block = new Block(color, 0, gposition);
+    if (color == 'G') {
+      $.each(theBlock.gPositions, function(index, position) {
+        var block = new Block(color, position);
         $('#scrambled').append(block.toHTML());
         console.log(block);
       });
     } else {
-      $.each(theBlock.bPositions, function(index, bposition) {
-        var block = new Block(color, bposition, 0);
+      $.each(theBlock.bPositions, function(index, position) {
+        var block = new Block(color, position);
         $('#scrambled').append(block.toHTML());
       });
     };
   });
-}
 
-function Block(color, bposition, gposition) {
+function Block(color, position) {
   this.color = color;
-  this.bposition = bposition;
-  this.gposition = gposition;
+  this.position = position;
   this.toHTML = function() {
-    return '<div id="square" class=' + this.color + '>' + this.color + this.position + '</div>';
-  }
+    return '<div id="square" class=' + this.color + '></div>';
+  };
 }
 
 $(document).ready(function() {
@@ -43,7 +33,7 @@ $(document).ready(function() {
  //   $('#scrambled').append('<div class="square"></div');    
  // }
  var creeper = new Creeper();
-})
+});
 
 //blck = 20
 //g = 44
